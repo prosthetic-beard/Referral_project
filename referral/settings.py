6fd9964c5600084701095a180e68eae3930c5844
yusufom <yusufolamilekanmuktar@gmail.com>
2023-12-07 10:29:07 +0100

    commit

--- referral/settings.py
+++ referral/settings.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'referral.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'readersh_shomedb',
+        'USER': 'readersh_readershome',
+        'PASSWORD': 'November-0411',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
